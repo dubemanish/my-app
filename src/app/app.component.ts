52bcee5cec3a5a11849a37da7f34b9ff7ac625c6
dubemanish <35660999+dubemanish@users.noreply.github.com>
2018-02-09 23:35:47 -0500

    Added myname in comment
    Added myname in comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my world ! This is my first App';
+  title = 'my world ! This is my first App developed by Manish';
 }
