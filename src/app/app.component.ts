e3160d8af294cbba948474f3141d05855fc76a76
dubemanish <35660999+dubemanish@users.noreply.github.com>
2018-02-10 23:42:27 -0500

    Update Comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my world ! This is my first App. This is Test for learning';
+  title = 'My world ! This is my first App. This is Test for learning';
 }
